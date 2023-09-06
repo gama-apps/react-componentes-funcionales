1853475a436f0113cd1a53c83b73a9e65572ce84
dev13 <dev13@codecraftdev.com>
2023-09-06 17:50:31 -0500

    cambios

--- src/components/FormSingUp.jsx
+++ src/components/FormSingUp.jsx
@@ -1,21 +1,25 @@
-import Button from '@mui/material/Button';
+import Button from "@mui/material/Button";
+import { TextField } from "@mui/material";
 
-function FormSingUp (){
-    return (
-        <form>
-            <label>Nombre</label>
-            <input type="text"/>
-            <label>Apelidos</label>
-            <input type="text"/>
-            <label>Correo Electronico</label>
-            <input type="email"/>
-            <label>Promociones</label>
-            <input type="checkbox"/>
-            <label>Novedades</label>
-            <input type="checkbox"/>
-            <Button variant="contained">Registrarse</Button>
-        </form>
-    )
+function FormSingUp() {
+  return (
+    <form>
+      <label>Nombre</label>
+      <input type="text" />
+      <TextField id="name" label="Nombre" variant="outlined" margin="normal"></TextField>
+      <TextField id="LastName" label="Apellidos" variant="outlined" margin="normal"></TextField>
+      <TextField id="Email" label="Email" variant="outlined" margin="normal"></TextField>
+      <label>Apelidos</label>
+      <input type="text" />
+      <label>Correo Electronico</label>
+      <input type="email" />
+      <label>Promociones</label>
+      <input type="checkbox" />
+      <label>Novedades</label>
+      <input type="checkbox" />
+      <Button variant="contained">Registrarse</Button>
+    </form>
+  );
 }
 
-export default FormSingUp
+export default FormSingUp;
